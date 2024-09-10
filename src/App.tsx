327f4feed737498c5444ac070d59e4e872c5d574
Romaric Zounlome <rzounlom@gmail.com>
2024-09-10 08:34:48 -0400

    refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container, Nav, Navbar } from "react-bootstrap";
 
-function App() {
-  const [count, setCount] = useState(0)
+import ConditionalRendering from "./components/conditional-rendering/ConditionalRendering";
+import Counter from "./components/counter/Counter";
+import { FC } from "react";
+import Input from "./components/input/Input";
 
+// import Example4TodoApp from './components/Example4TodoApp';
+// import Example5FetchData from './components/Example5FetchData';
+// import Example6ToggleTheme from './components/Example6ToggleTheme';
+
+const App: FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <Navbar bg="dark" variant="dark" sticky="top">
+        <Container>
+          <Navbar.Brand href="#home">React Fundamentals</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#counter">Counter</Nav.Link>
+            <Nav.Link href="#toggle-theme">Toggle Theme</Nav.Link>
+            <Nav.Link href="#conditional-rendering">
+              Conditional Rendering
+            </Nav.Link>
+            <Nav.Link href="#todo">Todo App</Nav.Link>
+            <Nav.Link href="#input">Input State</Nav.Link>
+            <Nav.Link href="#fetch">Fetch Data</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
-export default App
+      <section id="counter" style={styles.section}>
+        <Counter />
+      </section>
+      <section id="toggle-theme" style={styles.section}>
+        {/* <Example6ToggleTheme /> */}
+      </section>
+      <section id="conditional-rendering" style={styles.section}>
+        <ConditionalRendering name="John Doe" />
+      </section>
+      <section id="todo" style={styles.section}>
+        {/* <Example4TodoApp /> */}
+      </section>
+      <section id="input" style={styles.section}>
+        <Input />
+      </section>
+      <section id="fetch" style={styles.section}>
+        {/* <Example5FetchData /> */}
+      </section>
+    </div>
+  );
+};
+
+const styles = {
+  section: {
+    height: "75vh",
+    width: "100vw",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderBottom: "3px solid #ddd",
+  },
+};
+
+export default App;
