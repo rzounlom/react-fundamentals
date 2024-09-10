381a195c65287b642ff68b7d1a3179842ccd33ac
Romaric Zounlome <rzounlom@gmail.com>
2024-09-10 08:34:00 -0400

    add input example

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,33 @@
+// src/components/Example2InputState.tsx
+import { FC, useState } from "react";
+
+import { Form } from "react-bootstrap";
+
+const Input: FC = () => {
+  const [inputValue, setInputValue] = useState<string>("");
+
+  // Updates state as user types in input field
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Controlled Input Example</h2>
+      <Form>
+        <Form.Group controlId="formBasicInput">
+          <Form.Label>Enter Text</Form.Label>
+          <Form.Control
+            type="text"
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="Type something..."
+          />
+        </Form.Group>
+      </Form>
+      <p>Current Input: {inputValue}</p>
+    </div>
+  );
+};
+
+export default Input;
